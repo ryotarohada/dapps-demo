11f3042a95ef8e245c33f751a4384e1d98f9fcc8
Ryotaro Hada <67140275+ryotarohada@users.noreply.github.com>
2022-10-07 22:09:48 +0900

    Update hardhat.config.ts

--- smart_contract/hardhat.config.ts
+++ smart_contract/hardhat.config.ts
@@ -8,12 +8,10 @@ const config: HardhatUserConfig = {
   solidity: "0.8.17",
   networks: {
     goerli: {
-      url: "https://eth-goerli.g.alchemy.com/v2/CYNtuD_pUophtdXQEtb6CQqptLRMPZvw",
-      accounts: [
-        "95890a05aef3bd9c10731524e881e0b9dbd1c82a1abacf23e63baf60ce8b13db"
-      ]
-    }
-  }
+      url: "",
+      accounts: [""],
+    },
+  },
 };
 
 export default config;
